211564308af42dd0361bd710b33b3cf0f7c5d7f9
Ronald Pereira <ronald.drp11@gmail.com>
2015-07-24 15:27:20 -0300

    Create 1006.py

---
+++ 1006.py
@@ -0,0 +1,22 @@
+/*
+* Author : Ronald Pereira
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+*/
+
+a = float(input())
+b = float(input())
+c = float(input())
+ 
+media = ((a * 2) + (b * 3) + (c * 5)) / 10
+ 
+print("MEDIA = %.1f" %media)
