37d417174d2bc1e5ff0d101367d8758f60197eac
Ronald Pereira <ronald.drp11@gmail.com>
2015-07-24 15:26:09 -0300

    Create 1009.c

---
+++ 1009.c
@@ -0,0 +1,22 @@
+/*
+* Author : Ronald Pereira
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+*/
+
+nome = raw_input()
+salario = float(input())
+bonus = float(input())
+ 
+total = salario + (0.15 * bonus)
+ 
+print("TOTAL = R$ %.2f" %total)
